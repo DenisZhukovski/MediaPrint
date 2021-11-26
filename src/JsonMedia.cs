53bae22223de9ce95a5917bf736dc8faba18bd2c
Denis Zhukovski <dzhukovsky@softek.by>
2021-11-26 08:34:33 +0300

    Extract JValue object from JsonMedia

--- src/JsonMedia.cs
+++ src/JsonMedia.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace MediaPrint
 {
     public class JsonMedia : IMedia
     {
-        private readonly JObject _jObject;
+        internal readonly JObject _jObject;
         private readonly Formatting _formattig;
         private readonly JsonSerializerSettings _jsonSerializerSettings;
 
@@ -43,51 +41,12 @@ namespace MediaPrint
 
         public IMedia Put(string name, object value)
         {
-            if (value == null)
-            {
-                _jObject[name] = null;
-            }
-            else if (value is JToken jObject)
-            {
-                _jObject[name] = jObject;
-            }
-            else if (value is JsonMedia jsonMedia)
-            {
-                _jObject[name] = jsonMedia._jObject;
-            }
-            else if (value is IPrintable printable)
-            {
-                _jObject[name] = printable.ToJson()._jObject;
-            }
-            else
-            {
-                if (value is IEnumerable items && !(value is string) && !(value is IDictionary))
-                {
-                    var array = new JArray();
-                    foreach (var item in items)
-                    {
-                        array.Add(item.ToString());
-                    }
-                    _jObject[name] = array;
-                }
-                else if (value is IDictionary dictionary)
-                {
-                    _jObject[name] = JObject.Parse(JsonConvert.SerializeObject(dictionary, _formattig, _jsonSerializerSettings));
-                }
-                else
-                {
-                    string valueAsString;
-                    if (value is DateTime)
-                    {
-                        valueAsString = ((DateTime)value).ToString(_jsonSerializerSettings.DateFormatString);
-                    }
-                    else
-                    {
-                        valueAsString = value.ToString();
-                    }
-                    _jObject.Add(new JProperty(name, valueAsString));
-                }
-            }
+            _jObject.Add(
+                new JProperty(
+                    name,
+                    new Core.JValue(value, _formattig, _jsonSerializerSettings).Value()
+                )
+            );
             return this;
         }
 
